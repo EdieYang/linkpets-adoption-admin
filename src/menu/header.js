bf69b6fe78372593bed1342dffcf304f673c3d56
lookluke <haojie_look@163.com>
2019-09-12 11:31:14 +0800

    设置菜单及菜单图标

--- src/menu/header.js
+++ src/menu/header.js
@@ -1,17 +1,7 @@
 // 菜单 顶栏
 export default [
   { path: '/index', title: '首页', icon: 'home' },
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page2', title: '页面 2' },
-      { path: '/page3', title: '页面 3' }
-    ]
-  },
-  { path: '/orgManage/index', title: '组织管理', icon: 'folder-o' },
-  { path: '/orgManage/index', title: '组织管理', icon: 'folder-o' },
-  { path: '/adoptRelease/index', title: '领养发布', icon: 'folder-o' },
-  { path: '/activityRelease/index', title: '活动发布', icon: 'folder-o' }
+  { path: '/orgManage/index', title: '组织管理', icon: 'puzzle-piece' },
+  { path: '/adoptRelease/index', title: '领养发布', icon: 'flask' },
+  { path: '/activityRelease/index', title: '活动发布', icon: 'tachometer' }
 ]
